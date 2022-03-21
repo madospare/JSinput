439616741216a4496d5897670fc324f21d2ed77d
madospare <hundepopp@gmail.com>
2022-03-21 15:26:34 +0100

    commit 5

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ form.addEventListener("submit", function(event) {
     var navn = data.navn.value;
     var alder = data.alder.value;
     var drikke = data.drikke.value;
-    var mat = data.mat.value;
-    var matTyper = "";
+    var mat = data.mat;
+    var matTyper = [];
+    var lag = data.lag.value;
+    var dato = data.dato.value;
 
     for (var i = 0; mat[i]; i++) 
     {
         if (mat[i].checked) 
         {
-            matTyper = mat[i].value;
-            break;
+            matTyper.push(" " + mat[i].value);
         }
     }
-
-    output.innerHTML = "Hei, jeg heter " + navn + "! Jeg er " + alder + " år og liker å drikke " + drikke + ". Jeg liker å spise " + matTyper + ".";
+    
+    output.innerHTML = "Hei, jeg heter " + navn + " og er " + alder + " år. Jeg liker å drikke " + drikke + " og spise" + matTyper + ". Som løk har jeg mange lag. Jeg har akkurat " + lag + " lag. Jeg eksisterte kun én gang, og det var " + dato + ".";
 
     event.preventDefault();
 
