b04f0d312710fff4bcc7e3761a48565fabbc19db
madospare <hundepopp@gmail.com>
2022-03-21 11:17:02 +0100

    commit 3

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 var form = document.getElementById("form");
-var output = document.getElementById("tekst");
+var output = document.getElementById("output");
 
 form.addEventListener("submit", function(event) {
 
     var data = form.elements;
     var navn = data.navn.value;
+    var alder = data.alder.value;
+    var drikke = data.drikke.value;
+    var mat = data.mat.value;
+    var matTyper = "";
 
-    output.innerHTML = "Hei, jeg heter " + navn + "!";
+    for (var i = 0; mat[i]; i++) 
+    {
+        if (mat[i].checked) 
+        {
+            matTyper = mat[i].value;
+            break;
+        }
+    }
 
-    console.log("Hei, jeg heter " + navn + "!")
+    output.innerHTML = "Hei, jeg heter " + navn + "! Jeg er " + alder + " år og liker å drikke " + drikke + ". Jeg liker å spise " + matTyper + ".";
 
     event.preventDefault();
 
